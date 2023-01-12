98a36269293f91747e3483f3ad0bf6201496af5a
Jackeagle <jackeagle102@gmail.com>
2023-01-12 13:15:55 -0500

    sunfish: Drop HbmSVManager and DeviceIntelligenceNetworkPrebuilt
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- sunfish-vendor.mk
+++ sunfish-vendor.mk
@@ -516,7 +516,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel4 \
     DiagMon \
     EuiccGoogle \
@@ -537,7 +536,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     OBDM_Permissions \
     PixelNfc \
